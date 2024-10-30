388252917762eadba54aaddcb863ba0c5bde3b0c
Ange-Clement AKAZAN <114612394+AngeClementAkazan@users.noreply.github.com>
2024-10-30 12:22:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README", 'r') as f:
 setup( 
     name='Forest FLow-Based Per Variable Sampling',
    version='1.1.0',
-   description='This package implement a modified version of flow matching that is based on a per-variable sampling (generation) instead of generating the full data',
+   description='This package implements a modified version of flow matching that is based on a per-variable sampling (generation) instead of generating the full data',
    license="MIT",
     packages=find_packages(),
    long_description=long_description,
    author='Ange-Clément Akazan',
    author_email='ange-clement.akazan@mila.quebec',
-   url="https://github.com/AngeClementAkazan/Forest-FLow-Based-Variable-Sampling",
+   url="https://github.com/AngeClementAkazan/Sequential-FeatureForestFlow",
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -20,5 +20,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
     ],
-   install_requires=['numpy','pandas','scikit-learn','pandas','xgboost>=2.0.0'] #external packages as dependencies
+   install_requires=['numpy','pandas','scikit-learn','pandas','xgboost>=2.0.0','pot'] #external packages as dependencies
    )
